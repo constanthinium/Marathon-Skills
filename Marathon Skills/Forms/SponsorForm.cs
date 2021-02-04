bcaaafaf40a3fe8eb73e7b116661c3382e5b2d37
constanthinium <constanthinium@gmail.com>
2021-02-04 06:45:25 +0300

    Добавление бэкенда (не полного) формы спонсора
    Добавлена загрузка комбобокса из БД, валидация введенных значений, добавлено управление суммой пожертвований. Изменены некоторые элементы управления на более подходящие

--- Marathon Skills/Forms/SponsorForm.cs
+++ Marathon Skills/Forms/SponsorForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Marathon_Skills.Forms
@@ -11,11 +13,69 @@ namespace Marathon_Skills.Forms
 
             var time = new DateTime(2016, 11, 24, 6, 0, 0) - DateTime.Now;
             label10.Text = $"{time.Days} дней {time.Hours} часов и {time.Minutes} минут до старта марафона!";
+
+            var con = new SqlConnection(
+                @"Server=localhost\SQLEXPRESS;Database=MarathonSkills;Trusted_Connection=True;");
+            var adapter = new SqlDataAdapter(@"
+select concat(FirstName, ', ', LastName, ' - ', BibNumber, ' (', Runner.CountryCode, ')') as Runner
+from Runner
+join [User] on Runner.Email = [User].Email
+join Registration on Runner.RunnerId = Registration.RunnerId
+join RegistrationEvent on Registration.RegistrationId = RegistrationEvent.RegistrationId
+            ", con);
+            con.Open();
+            var set = new DataSet();
+            adapter.Fill(set);
+
+            comboBox1.DataSource = set.Tables[0];
+            comboBox1.ValueMember = "Runner";
         }
 
         private void Submit(object sender, EventArgs e)
         {
-            
+            if (placeholderTextBox1.Text == "" || placeholderTextBox1.Text == "")
+            {
+                MessageBox.Show("Поля имя и владелец карты не могут быть пустыми");
+                return;
+            }
+
+            if (!maskedTextBox1.MaskFull)
+            {
+                MessageBox.Show("Номер карты должен быть полностью заполнен");
+                return;
+            }
+
+            if (!maskedTextBox2.MaskFull || !maskedTextBox3.MaskFull ||
+                new DateTime(int.Parse(maskedTextBox3.Text), int.Parse(maskedTextBox2.Text), 0) < DateTime.Now)
+            {
+                MessageBox.Show("Месяц и год срока действия должны быть заполнены, а также быть меньше, чем сегодняшняя дата");
+                return;
+            }
+
+            if (!maskedTextBox4.MaskFull)
+            {
+                MessageBox.Show("CVC должен содержать 3 цифры");
+                return;
+            }
+        }
+
+        private void roundedButton2_Click(object sender, EventArgs e)
+        {
+            var labelText = label14.Text;
+            label14.Text = int.Parse(labelText.Substring(0, labelText.IndexOf('$'))) + 10 + "$";
+        }
+
+        private void roundedButton1_Click(object sender, EventArgs e)
+        {
+            var val = int.Parse(label14.Text.Substring(0, label14.Text.IndexOf('$')));
+            if (val > 10)
+                label14.Text = val - 10 + "$";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out _))
+                label14.Text = textBox1.Text + '$';
         }
     }
 }
